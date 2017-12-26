d1d1507b8a1dee904bed0efd3527c46a6e574e73
Lasse Loeffler <dayaftereh@gmail.com>
2017-12-26 17:26:50 +0100

    fixed issue with map

--- led.ino
+++ led.ino
@@ -3,7 +3,8 @@ int _red_value = 0;
 int _green_value = 0;
 int _blue_value = 0;
 
-void led_init() {
+void led_init()
+{
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
   pinMode(LED_BLUE_PIN, OUTPUT);
@@ -13,7 +14,8 @@ void led_init() {
   led_status_off();
 }
 
-void led_print_gpios(){
+void led_print_gpios()
+{
   Serial.print("led gpio: [ red: ");
   Serial.print(LED_RED_PIN);
   Serial.print(", green: ");
@@ -26,31 +28,36 @@ void led_print_gpios(){
   Serial.println(LED_STATUS);
 }
 
-int led_value_to_pwm(int value){
-  float x = value / ((float)LED_RANGE);
-  int pwm_value = (int)(x * PWMRANGE);
+int led_value_to_pwm(int value)
+{
+  int pwm_value = map(value, 0, LED_RANGE, 0, PWMRANGE);
   return pwm_value;
 }
 
-void led_set(int red, int green, int blue){
+void led_set(int red, int green, int blue)
+{
   _red_value = red;
   _green_value = green;
   _blue_value = blue;
 }
 
-int led_red(){
+int led_red()
+{
   return _red_value;
 }
 
-int led_green(){
+int led_green()
+{
   return _green_value;
 }
 
-int led_blue(){
+int led_blue()
+{
   return _blue_value;
 }
 
-void led_write_values(){
+void led_write_values()
+{
   int red_pwm = led_value_to_pwm(_red_value);
   analogWrite(LED_RED_PIN, red_pwm);
 
@@ -61,11 +68,12 @@ void led_write_values(){
   analogWrite(LED_BLUE_PIN, blue_pwm);
 }
 
-void led_status_off(){
+void led_status_off()
+{
   digitalWrite(LED_STATUS, HIGH);
 }
 
-void led_status_on(){
+void led_status_on()
+{
   digitalWrite(LED_STATUS, LOW);
 }
-
